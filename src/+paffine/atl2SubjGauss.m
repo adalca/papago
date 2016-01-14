09bd9c98daa15edcfe52d98e5303f662b6376c25
adalca <adalca@mit.edu>
2016-01-14 18:33:40 -0500

    moving atl into to R computation inside atl2subjGauss

--- src/+paffine/atl2SubjGauss.m
+++ src/+paffine/atl2SubjGauss.m
@@ -1,36 +1,115 @@
-function [subjMu, subjSigma] = atl2SubjGauss(atlMu, atlSigma, R, method, regVal)
+function [subjMu, subjSigma, varargout] = atl2SubjGauss(atlMu, atlSigma, method, varargin)
 % transform gaussian parameters from atlas space to subject space using a interpolation matrix R
 % computed via cor2interpmat
+%
+% [subjMu, subjSigma] = atl2SubjGauss(atlMu, atlSigma, method, R, <regVal>)
+%
+% [subjMu, subjSigma] = atl2SubjGauss(atlMu, atlSigma, method, atlLoc, atlPatchSize, atlLoc2SubjSpace, regVal or subjLoc2AtlSpace)
 %
 % atlMu is a N-by-1
 % atlSigma is a N-by-N matrix
 % R is M-by-N if inverse (taking atlas to subject space), or N-by-M if forward (taking subject to
 %   altas space)
-% method is 'inverse' of 'forward'. 
+% method is 'forward', 'forwardModeling', 'forwardModel', 'inverse', 'inverseModeling', 'inverseModel'
 % <regVal> is a diagonal regularization value for sigma: scalar of N-by-1.
 %
 % returnes subjMu and subjAtls are M-by-1 and M-by-M, respectively.
 %
 % part of papago
 
-
-    narginchk(4, 5);
+    % input checking and output preparation
+    narginchk(4, 7);
     
+    varargout = cell(1, nargout - 2);
+
+    % determine direction to execute
     switch method
-        case {'forward', 'modelingFwd', 'forward-model'}
-            subjSigma = pinv(R) * atlSigma * pinv(R');
-            subjMu = pinv(R) * atlMu(:); 
-            
-            % regularize if desired
-            if nargin >= 5
-                subjSigma = subjSigma + eye(size(subjSigma)) .* regVal;
-            end
-            % newSigma = nearestSPD(subjSigma);
-            
-        case {'inverse', 'modelingInv', 'inverse-model'}
-            subjSigma = R * atlSigma * R';
-            subjMu = R * atlMu(:);
-            
+        case {'forward', 'forwardModeling', 'forwardModel'}
+            atl2SubjFcn = @atl2SubjFwd;
+
+        case {'inverse', 'inverseModeling', 'inverseModel'}
+            atl2SubjFcn = @atl2SubjBwd;
+    
         otherwise
             error('not a valid method for sigma estimation');
     end
+    
+    % call transforming function
+    [subjMu, subjSigma, varargout{:}] = atl2SubjFcn(atlMu, atlSigma, varargin{:});
+end
+    
+function [subjMu, subjSigma, varargout] = atl2SubjFwd(atlMu, atlSigma, varargin)
+% [subjMu, subjSigma] = atl2SubjFwd(atlMu, atlSigma, R, <regVal>)
+% or
+% [subjMu, subjSigma, subbjmask, subjPatchMins, subjPatchSize] = 
+%   atl2SubjFwd(atlMu, atlSigma, atlLoc, atlPatchSize, atlLoc2SubjSpace, <regVal>)
+
+    needr = nargin > 4;
+    
+    % obtain subj2AtlR: R subj --> atl. R is |atl|-by-|subj|
+    if needr 
+        warning('forward in currently untested!');
+        [atlLoc, atlPatchSize, atlLoc2SubjSpace] = varargin{1:3};
+        [~, subjPatchMins, subjPatchSize] = paffine.atl2SubjPatch(atlLoc, atlPatchSize, atlLoc2SubjSpace);
+
+        atlPatchRange = arrayfunc(@(a, p) a:p, atlLoc, atlLoc + atlPatchSize - 1);
+        atlLoc2SubjSpacePatch = extractAndNormalizePatchCor(atlLoc2SubjSpace, atlPatchRange, subjPatchMins);
+        [R, subjMask, ~] = cor2interpmat(subjPatchSize, atlLoc2SubjSpacePatch);
+        
+    else
+        R = varargin{:};
+    end
+
+    % get subject Gaussian parameters from atlas Gaussian Parameters
+    subjSigma = pinv(R) * atlSigma * pinv(R');
+    subjMu = pinv(R) * atlMu(:); 
+
+    % regularize if desired
+    if (needvar && numel(varargin) == 4) || (~needvar && numel(varargin) == 2)
+        regVal = varargin{end};
+        subjSigma = subjSigma + eye(size(subjSigma)) .* regVal;
+    end
+    % newSigma = nearestSPD(subjSigma);
+    
+    % prepare extra outputs
+    if nargout > 2
+        assert(needr, 'can only ask for more outputs when computing R');
+        varargout = {subjMask, subjPatchMins, subjPatchSize};
+        varargout = varargout(1:(nargout - 2));
+    end
+end
+
+function [subjMu, subjSigma, varargout] = atl2SubjBwd(atlMu, atlSigma, varargin)
+% [subjMu, subjSigma] = atl2SubjBwd(atlMu, atlSigma, R)
+% or
+% [subjMu, subjSigma, subbjmask, subjPatchMins, subjPatchSize] = 
+%   atl2SubjBwd(atlMu, atlSigma, atlLoc, atlPatchSize, atlLoc2SubjSpace, subjLoc2AtlSpace)
+
+    needr = nargin > 3;
+
+    % obtain atl2SubjR: R atl --> subj. R is |subj|-by-|atl|
+    if needr 
+        [atlLoc, atlPatchSize, atlLoc2SubjSpace, subjLoc2AtlSpace] = varargin{:};
+        [subjPatchRange, subjPatchMins, subjPatchSize] = paffine.atl2SubjPatch(atlLoc, atlPatchSize, atlLoc2SubjSpace);
+        subjLoc2AtlSpacePatch = extractAndNormalizePatchCor(subjLoc2AtlSpace, subjPatchRange, atlLoc);
+        [R, ~, subjMask] = cor2interpmat(atlPatchSize, subjLoc2AtlSpacePatch);
+    else
+        R = varargin{:};
+    end
+
+    % go from atlas to subject via R
+    subjSigma = R * atlSigma * R';
+    subjMu = R * atlMu(:);
+    
+    % prepare extra outputs
+    if nargout > 2
+        assert(needr, 'can only ask for more outputs when computing R');
+        varargout = {subjMask, subjPatchMins, subjPatchSize};
+        varargout = varargout(1:(nargout - 2));
+    end
+end
+    
+function patchCor = extractAndNormalizePatchCor(volCor, range, mins)
+    patchCor = cellfunc(@(x) x(range{:}), volCor);
+    patchCor = cellfunc(@(x, m) x - m + 1, patchCor, mat2cellsplit(mins));
+end
--- src/+paffine/recon.m
+++ src/+paffine/recon.m
@@ -2,54 +2,27 @@ function [reconPatch, logp, subjPatchMins] = recon(atlMu, atlSigma, atlLoc, atlP
     subjVol, subjWeightVol, atlLoc2SubjSpace, method, varargin)
 % reconstruct a subject patch given the atlas
 %
+% varargin is:
+%   <regVal> if method is forward 
+%   subjLoc2AtlSpace if method is inverse
+%
 % srcLoc2TgtSpace is computed via
 %   srcLoc2TgtSpace = tform2cor3d(subj2Atl, size(subjVol), srcVoxDims, tgtVolSize, tgtVoxDims, dirn);
 %
 % TODO: use cropVolume to extract patch.
+% TODO: allow just tform as opposed to cell map?
 
-    % get the subject space coordinates
-    [subjPatchRange, subjPatchMins, subjPatchSize] = ...
-        paffine.atl2SubjPatch(atlLoc, atlPatchSize, atlLoc2SubjSpace);
-
-    switch method
-        case {'forward', 'modelingFwd', 'forward-model'}
-            regVal = varargin{1};
-            warning('untested');
-            
-            % obtain R subj --> atl. R is |atl|-by-|subj|
-            atlPatchRange = arrayfunc(@(a, p) a:p, atlLoc, atlLoc + atlPatchSize - 1);
-            atlLoc2SubjSpacePatch = extractAndNormalizePatchCor(atlLoc2SubjSpace, atlPatchRange, subjPatchMins);
-            [subj2AtlR, subjMask, ~] = cor2interpmat(subjPatchSize, atlLoc2SubjSpacePatch);
-            
-            % get the subject-space gaussian parameters
-            [subjMu, subjSigma] = paffine.atl2SubjGauss(atlMu, atlSigma, subj2AtlR, method, regVal);
-            
-        case {'inverse', 'modelingInv', 'inverse-model'}
-            subjLoc2AtlSpace = varargin{1};
-            
-            % obtain R atl --> subj. R is |subj|-by-|atl|
-            subjLoc2AtlSpacePatch = extractAndNormalizePatchCor(subjLoc2AtlSpace, subjPatchRange, atlLoc);
-            [atl2SubjR, ~, subjMask] = cor2interpmat(atlPatchSize, subjLoc2AtlSpacePatch);
-            
-            % get the subject-space gaussian parameters
-            [subjMu, subjSigma] = paffine.atl2SubjGauss(atlMu, atlSigma, atl2SubjR, method);
-            
-        otherwise
-            error('paffine: unknown recon method');
-    end
+    % get subject gaussian coordinates and information
+    [subjMu, subjSigma, subjInterpMask, subjPatchMins, subjPatchSize] = ...
+        paffine.atl2SubjGauss(atlMu, atlSigma, method, atlLoc, atlPatchSize, atlLoc2SubjSpace, varargin{:});
            
     % reconstruct the patch
-    subjPatch = subjVol(subjPatchRange{:});
-    subjWeightPatch = subjWeightVol(subjPatchRange{:});
-    [reconPatch, invBb] = paffine.reconSubjPatch(subjPatch, subjWeightPatch, subjMask, subjMu, subjSigma);
+    subjPatch = cropVolume(subjVol, subjPatchMins, subjPatchMins + subjPatchSize - 1);
+    subjWeightPatch = cropVolume(subjWeightVol, subjPatchMins, subjPatchMins + subjPatchSize - 1);
+    [reconPatch, invBb] = paffine.reconSubjPatch(subjPatch, subjWeightPatch, subjInterpMask, subjMu, subjSigma);
     
     % compute the logp
     if nargout > 1
-        logp = paffine.logpSubjPatch(subjPatch, subjWeightPatch, subjMask, subjMu, subjSigma, invBb);
+        logp = paffine.logpSubjPatch(subjPatch, subjWeightPatch, subjInterpMask, subjMu, subjSigma, invBb);
     end
-end
-
-function patchCor = extractAndNormalizePatchCor(volCor, range, mins)
-    patchCor = cellfunc(@(x) x(range{:}), volCor);
-    patchCor = cellfunc(@(x, m) x - m + 1, patchCor, mat2cellsplit(mins));
 end
