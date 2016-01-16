3f036280f053e802d8ac9b3e4b7ea5f3902d8ce2
Adrian Dalca <adalca@mit.edu>
2016-01-16 14:54:56 -0500

    clean up conditional reconstruction methods

--- sandbox/+xplr/conditionalReconstruction.m
+++ sandbox/+xplr/conditionalReconstruction.m
@@ -1,120 +1,109 @@
-%% initialize
+%% Test Conditional Reconstruction methods
+% initialize
 setup
 
 %% parameters
 atlPatchSize = ones(1, 3) * 9; 
 atlLoc = LOC_VENTRICLE_EDGE; %LOC_VENTRICLE_EDGE; % LOC_LEFT_CORTEX+10; %LOC_VENTRICLE_EDGE; %LOC_LEFT_CORTEX;
-
-gmmmethod = 'groundtruth'; % 'compute', 'load', 'groundtruth'
-method = 'inverse'; % 'forward', 'inverse'
+gmmK = 5; % 5 good for ventricle, 25 for cortex?
+crmethod = 'inverse'; % 'forward', 'inverse'
 regVal = 1e-4; % regulaization to the diagonal of subjSigma, if using method forward
+reconSubj = 3; %1, 3
+patchColPad = ones(1, 3) * 2;
 
-subject = 3; %1, 3
+%% load buckner volumes and prepare volume data
+% load ADNI full-subject, and buckner full-dataset column.
 
-%% load volumes
-% we'll use some local, small subset, of the buckner data for now.
+% load *ground truth* data column from buckner
+bucknermd = loadmd([SYNTHESIS_DATA_PATH, 'buckner', '_restor_md_*']);
+[bucknerIsoPatchCol, ~, volidx] = ...
+    subspacetools.md2patchcol(bucknermd, 'brainIso2Ds5Us5size', atlPatchSize, atlLoc, patchColPad);
 
-% TODO: should move this to using the existing md structure, 
-% once both katie and adrian have access to the same large data.
-bpref = sprintf('buckner%02d', subject);
+% load selected ADNI subject volumes
+adnimd = loadmd([SYNTHESIS_DATA_PATH, 'adni', '_restor_md_*']);
+dsSubjNii = adnimd.loadModality('brainDs5Us5', reconSubj);
+subjVol = double(dsSubjNii.img);
+subjWeightVol = logical(adnimd.loadVolume('brainDs5Us5Mask', reconSubj));
+dsSubjInAtlNii = adnimd.loadModality('brainDs5Us5Reg', reconSubj);
+subjInAtlTform = load(adnimd.getModality('brainDs5Us5RegMat', reconSubj));
 
-% subject space
-subjisonii = loadNii(sprintf('%s/%s/%s/%s_brain_cropped5.nii.gz', SYNTHESIS_DATA_PATH, 'buckner/proc', bpref, bpref)); % iso
-dsnii = loadNii(sprintf('%s/%s/%s/%s_brain_downsampled5_reinterpolated5.nii.gz', SYNTHESIS_DATA_PATH, 'buckner/proc', bpref, bpref)); % ds 
-dsmasknii = loadNii(sprintf('%s/%s/%s/%s_brain_downsampled5_reinterpolated5_dsmask.nii.gz', SYNTHESIS_DATA_PATH, 'buckner/proc', bpref, bpref));
-
-% atlas space (i.e. registered data)
-isoregnii = loadNii(sprintf('%s/%s/%s/%s_brain_iso_2_ds5_us5_size_reg.nii.gz', SYNTHESIS_DATA_PATH, 'buckner/proc', bpref, bpref)); % ds
-dsregnii = loadNii(sprintf('%s/%s/%s/%s_brain_downsampled5_reinterpolated5_reg.nii.gz', SYNTHESIS_DATA_PATH, 'buckner/proc', bpref, bpref)); % ds
-tf = load(sprintf('%s/%s/%s/%s_brain_downsampled5_reinterpolated5_reg.mat', SYNTHESIS_DATA_PATH, 'buckner/proc', bpref, bpref));
-dsmaskregnii = loadNii(sprintf('%s/%s/%s/%s_brain_downsampled5_reinterpolated5_dsmask_reg.nii.gz', SYNTHESIS_DATA_PATH, 'buckner/proc', bpref, bpref));
-    
-%% prepare necessary inputs for reconstruction
-% atlLoc, atlPatchSize, subjVol, subjWeightVol, atlLoc2SubjSpace, method
-subjVol = double(dsnii.img);
-subjWeightVol = logical(dsmasknii.img);
-
-subjDims = dsnii.hdr.dime.pixdim(2:4);
-atlDims = dsregnii.hdr.dime.pixdim(2:4);
-subjLoc2AtlSpace = tform2cor3d(tf.tform, size(subjVol), subjDims, size(dsregnii.img), atlDims);
-atlLoc2SubjSpace = tform2cor3d(tf.tform, size(subjVol), subjDims, size(dsregnii.img), atlDims, 'backward');
-extraReconArg = ifelse(strcmp(method, 'inverse'), subjLoc2AtlSpace, regVal);
+% prepare necessary inputs for conditional-based reconstruction
+subjDims = dsSubjNii.hdr.dime.pixdim(2:4);
+atlDims = dsSubjInAtlNii.hdr.dime.pixdim(2:4);
+tform = subjInAtlTform.tform;
+atlVolSize = size(dsSubjInAtlNii.img);
+subjLoc2AtlSpace = tform2cor3d(tform, size(subjVol), subjDims, atlVolSize, atlDims);
+atlLoc2SubjSpace = tform2cor3d(tform, size(subjVol), subjDims, atlVolSize, atlDims, 'backward');
+extraReconArg = ifelse(strcmp(crmethod, 'inverse'), subjLoc2AtlSpace, regVal);
 
 %% "groundtruth" testing
-% atlMu, atlSigma: done below
+% Test the ability to reconstruct a patch, where we compute a (regularized) covariance from the
+% single "true" (isotropic) subject patch that's hidden from the reconstruction method.
 
-% get the covariance from the (isotropic) patch we're looking at in atlas space and see if we can
-% reconstruction the right patch in subject space.
-isoSubjVolInAtl = double(isoregnii.img);
-
-% get mu and sigma in atlas space
-atlPatch = cropVolume(isoSubjVolInAtl, atlLoc, atlLoc + atlPatchSize - 1);
+% get "true" mu and sigma in atlas space
+isoSubjInAtlVol = adnimd.loadVolume('brainIso2Ds5Us5sizeReg', reconSubj);
+atlPatch = cropVolume(isoSubjInAtlVol, atlLoc, atlLoc + atlPatchSize - 1);
 atlMu = zeros(atlPatchSize);
 atlSigma = atlPatch(:) * atlPatch(:)' + eye(prod(atlPatchSize)) * regVal;
 
-% reconstruct
+% reconstruct the given patch
 reconPatch_gt = paffine.recon(atlMu, atlSigma, atlLoc, atlPatchSize, ...
-    subjVol, subjWeightVol, atlLoc2SubjSpace, method, extraReconArg);
-
-%% load data column
-% load *ground truth* data column
-atlasesDataSetup; % atlases
-BUCKNER_PATH_ORIG = fullfile(BUCKNER_PATH, 'orig'); % original files
-BUCKNER_PATH_PROC = fullfile(SYNTHESIS_DATA_PATH, 'buckner/proc'); % buckner processing 
-md = restorationmd(dsAmounts, BUCKNER_PATH_PROC, SYNTHESIS_DATA_PATH, 'buckner');
-[col, layeridx, volidx] = subspacetools.md2patchcol(md, 'brainIso2Ds5Us5size', atlPatchSize, atlLoc, [2, 2, 2]);
-col(volidx == subject, :) = [];
+    subjVol, subjWeightVol, atlLoc2SubjSpace, crmethod, extraReconArg);
 
 %% single - gaussian estimation
-gmm = fitgmdist(col, 1);
+gmm = fitgmdist(bucknerIsoPatchCol, 1);
 atlMu = gmm.mu(:)';
 atlSigma = gmm.Sigma + eye(prod(atlPatchSize)) * regVal;
 
 reconPatch_sg = paffine.recon(atlMu, atlSigma, atlLoc, atlPatchSize, ...
-    subjVol, subjWeightVol, atlLoc2SubjSpace, method, extraReconArg);
+    subjVol, subjWeightVol, atlLoc2SubjSpace, crmethod, extraReconArg);
 
 %% gaussian mixture model
 % learn a gaussian mixture model with K clusters from the *true* isotropic data, 
-% minus this subject.
-K = 5;
 gmmopt = statset('Display', 'iter', 'MaxIter', 20, 'TolFun', 0.001);
 
 % compute the gaussian mixture model
 tic
-gmm = fitgmdist(col, K, 'regularizationValue', regVal, 'replicates', 10, 'Options', gmmopt);
+gmm = fitgmdist(bucknerIsoPatchCol, gmmK, 'regularizationValue', regVal, 'replicates', 10, 'Options', gmmopt);
 fprintf('Gaussian mixture model took %3.3f sec\n', toc);
 
 % reconstruct
 % TODO: really, we should only be computing logp, not the reconstruction!
-logps = zeros(1, K);
-reconPatches = cell(1, K);
-subjPatchLocs = cell(1, K);
-for k = 1:K
+logps = zeros(1, gmmK);
+logp2 = zeros(1, gmmK);
+reconPatches = cell(1, gmmK);
+subjPatchLocs = cell(1, gmmK);
+for k = 1:gmmK
     atlMu = gmm.mu(k, :)';
     atlSigma = gmm.Sigma(:, :, k) + eye(prod(atlPatchSize)) * regVal;
     
-    [reconPatches{k}, logps(k), subjPatchLocs{k}] = paffine.recon(atlMu, atlSigma, atlLoc, atlPatchSize, ...
-        subjVol, subjWeightVol, atlLoc2SubjSpace, method, extraReconArg);
+    [reconPatches{k}, subjPatchLocs{k}, logps(k)] = paffine.recon(atlMu, atlSigma, atlLoc, ...
+        atlPatchSize, subjVol, subjWeightVol, atlLoc2SubjSpace, crmethod, extraReconArg);
 end
 
 % get the optimal patch reconstruction
 [~, clust] = max(log(gmm.ComponentProportion) + logps);
 reconPatch_kg = reconPatches{clust};
-subjPatchLoc = subjPatchLocs{clust};
 
-%% visualize and compare
-subjPatchSize = size(reconPatch_gt);
+%% get some useful subject-space data for comparison or initialization
+% get the subject space coordinates
+[~, subjPatchLoc, subjPatchSize] = ...
+    paffine.atl2SubjPatch(atlLoc, atlPatchSize, atlLoc2SubjSpace);
 
 % compare subject patches for: true isotropic volume, linear interpolation, mask.
-dsSubjPatch = cropVolume(dsnii.img, subjPatchLoc, subjPatchLoc + subjPatchSize - 1);
+dsSubjPatch = cropVolume(subjVol, subjPatchLoc, subjPatchLoc + subjPatchSize - 1);
 dsSubjPatch(isnan(reconPatch_gt)) = nan;
-maskSubjPatch = cropVolume(dsmasknii.img, subjPatchLoc, subjPatchLoc + subjPatchSize - 1);
-maskSubjPatch(isnan(reconPatch_gt)) = nan;
-correctSubjPatch = cropVolume(subjisonii.img, subjPatchLoc, subjPatchLoc + subjPatchSize - 1);
+maskSubjPatch = cropVolume(subjWeightVol, subjPatchLoc, subjPatchLoc + subjPatchSize - 1);
+maskSubjPatch(isnan(reconPatch_gt)) = false;
+
+isoSubjVol = adnimd.loadVolume('brainCropped5', reconSubj);
+correctSubjPatch = cropVolume(isoSubjVol, subjPatchLoc, subjPatchLoc + subjPatchSize - 1);
 correctSubjPatch(isnan(reconPatch_gt)) = nan;
 
+%% visualize and compare
+
 % 3D visualization
-view3Dopt(correctSubjPatch, maskSubjPatch, dsSubjPatch, ...
+view3Dopt(correctSubjPatch, maskSubjPatch*1, dsSubjPatch, ...
     reconPatch_gt, reconPatch_sg, reconPatch_kg);
 
 % 2D visualization
@@ -127,4 +116,15 @@ subplot(2, 3, 5); imagesc(patchview.reshapeto2D(reconPatch_sg));   title('our re
 subplot(2, 3, 6); imagesc(patchview.reshapeto2D(reconPatch_kg));   title('our recon gmm');
 colormap gray;
 
-%% TODO: compare logp in this manner with others.
+%% 3D visualize of the gmm options, ssd to "true" data, and logp based on sparse data.
+fprintf('logP  :');
+fprintf('%9.2f ', logps);
+fprintf('\nlogPv2:');
+fprintf('%9.2f ', logp2);
+fprintf('\nssd   :');
+valid = ~isnan(correctSubjPatch);
+ssds = cellfun(@(x) ssd(x(valid), correctSubjPatch(valid)), reconPatches);
+fprintf('%9.2f ', ssds);
+newline;
+
+view3Dopt(correctSubjPatch, reconPatches{:})
