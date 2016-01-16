537edc3212ad1bc4b532d3c27ff5561883561545
Adrian Dalca <adalca@mit.edu>
2016-01-16 15:02:08 -0500

    adding papago subvolume reconstruction

---
+++ src/+papago/subvolRecon.m
@@ -0,0 +1,89 @@
+function [reconPatches, reconLocs] = subvolRecon(gmm, subvolLoc, subvolSize, atlPatchSize, crmethod, ...
+    dsSubjInAtl, dsSubjInAtlMask, dsSubj, dsSubjWeight, varargin)
+% reconstruct all patches within a subvolume
+% 
+% subvolRecon(gmm, subvolloc, subvolSize, atlPatchSize, crmethod, dsSubjInAtlVol, dsSubjInAtlMaskVol, subjVol, subjWeightVol, atlLoc2SubjSpace, subjLoc2AtlSpace)
+%
+% subvolRecon(gmm, subvolloc, subvolSize, atlPatchSize, crmethod, dsSubjInAtlNii, dsSubjInAtlMaskNii, dsSubjNii, dsSubjWeightNii, tform)
+%   
+% Currently uses atlas-space heuristic to get the optimal cluster and the conditional rotation
+% reconstruction method.
+
+    % need: subjVol, subjWeightVol, crmethod, subjLoc2AtlSpace or regVal
+    [dsSubjInAtlVol, dsSubjInAtlMaskVol, dsSubjVol, dsSubjWeightVol, atlLoc2SubjSpace, extraReconArg] ...
+        = parseInputs(crmethod, dsSubjInAtl, dsSubjInAtlMask, dsSubj, dsSubjWeight, varargin{:});
+
+    % prepare atlas locations to loop over
+    atlLocs = patchlib.grid(subvolSize, atlPatchSize, 'sliding', 'sub');
+    atlLocs = cellfunc(@(x) x(:), atlLocs);
+    atlLocs = cat(2, atlLocs{:});
+    
+    % prepare reconstructions
+    nLocs = size(atlLocs, 1);
+    reconPatches = cell(nLocs, 1);
+    reconLocs = cell(nLocs, 1);
+    
+    % go through each patch and reconstruct
+    for i = 1:nLocs
+        atlLoc = subvolLoc + atlLocs(i, :);
+
+        % extract atlas patch
+        dsAtlPatch = cropVolume(dsSubjInAtlVol, atlLoc, atlLoc + atlPatchSize - 1);
+        dsAtlMaskPatch = cropVolume(dsSubjInAtlMaskVol, atlLoc, atlLoc + atlPatchSize - 1);
+        
+        % choose optimal cluster using the in-atlas heuristic measure
+        K = size(gmm.mu, 1);
+        logp = zeros(1, K);
+        for k = 1:K
+            atlMu = gmm.mu(k, :)';
+            atlSigma = gmm.Sigma(:, :, k);
+            logp(k) = logmvnpdf(dsAtlPatch(:)' .* dsAtlMaskPatch(:)', atlMu(:)' .* dsAtlMaskPatch(:)', atlSigma);
+        end
+        
+        % get the optimal cluster via posteriors
+        [~, optk] = max(log(gmm.ComponentProportion) + logp);
+        
+        % reconstruct in subject space
+        atlMu = gmm.mu(optk, :)';
+        atlSigma = gmm.Sigma(:, :, optk);
+        [reconPatches{i}, reconLocs{i}] = paffine.recon(atlMu, atlSigma, atlLoc, atlPatchSize, ...
+            dsSubjVol, dsSubjWeightVol, atlLoc2SubjSpace, crmethod, extraReconArg);
+    end 
+end
+
+function [subjAtlVol, dsSubjInAtlMaskVol, dsSubjVol, dsSubjWeightVol, atlLoc2SubjSpace, extraReconArg] ...
+    = parseInputs(crmethod, dsSubjInAtl, dsSubjInAtlMask, dsSubj, dsSubjWeight, varargin)
+
+    if iscell(varargin{1})
+        subjAtlVol = dsSubjInAtl;
+        dsSubjInAtlMaskVol = dsSubjInAtlMask;
+        dsSubjVol = dsSubj;
+        dsSubjWeightVol = dsSubjWeight;
+        atlLoc2SubjSpace = varargin{1};
+        
+        extraReconArg = [];
+        if numel(varargin) > 1
+            extraReconArg = varargin{2};
+        end 
+        
+    else
+        dsSubjInAtlNii = dsSubjInAtl;
+        subjAtlVol = dsSubjInAtlNii.img;
+        dsSubjInAtlMaskVol = dsSubjInAtlMask;
+        dsSubjNii = dsSubj;
+        dsSubjVol = dsSubjNii.img;
+        dsSubjWeightVol = dsSubjWeight;
+        
+        tform = varargin{1};
+        regVal = 0;
+        if numel(varargin) > 1
+            regVal = varargin{2};
+        end 
+        
+        [subjLoc2AtlSpace, atlLoc2SubjSpace] = nii2cor3d(tform, dsSubjNii, dsSubjInAtlNii);
+        extraReconArg = ifelse(strcmp(crmethod, 'inverse'), subjLoc2AtlSpace, regVal);
+    end
+end
+
+
+
