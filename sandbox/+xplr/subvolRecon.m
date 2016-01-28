02c7b2a81ab3357cf0aa508acc4cd67ab2d7f564
Adrian Dalca <adalca@mit.edu>
2016-01-28 01:47:50 -0500

    improve subvolRecon

--- sandbox/+xplr/subvolRecon.m
+++ sandbox/+xplr/subvolRecon.m
@@ -4,31 +4,35 @@ setup
 
 %% parameters
 atlPatchSize = ones(1, 3) * 9; 
-atlLoc = LOC_VENTRICLE_EDGE-10; %LOC_VENTRICLE_EDGE; % LOC_LEFT_CORTEX+10; %LOC_VENTRICLE_EDGE; %LOC_LEFT_CORTEX;
-gmmK = 5; % 5 good for ventricle, 25 for cortex?
+atlLoc = LOC_VENTRICLE_EDGE; %LOC_VENTRICLE_EDGE; % LOC_LEFT_CORTEX+10; %LOC_VENTRICLE_EDGE; %LOC_LEFT_CORTEX;
+gmmK = 15; % 5 good for ventricle, 25 for cortex?
 crmethod = 'inverse'; % 'forward', 'inverse'
 regVal = 1e-4; % regulaization to the diagonal of subjSigma, if using method forward
-reconSubj = 3; %1, 3
+reconSubj = 5; %1, 3
 patchColPad = ones(1, 3) * 2;
+
+% train and test datasets
+traindataset = 'adni';
+testdataset = 'buckner';
 
 %% load buckner volumes and prepare volume data
 % load ADNI full-subject, and buckner full-dataset column.
 
 % load *ground truth* data column from buckner
-fnames = fullfile(SYNTHESIS_DATA_PATH, 'buckner', 'md', [sys.usrname, '_restor_md_*']);
-bucknermd = loadmd(fnames);
+fnames = fullfile(SYNTHESIS_DATA_PATH, traindataset, 'md', [sys.usrname, '_restor_md_*']);
+trainmd = loadmd(fnames);
 [bucknerIsoPatchCol, ~, volidx] = ...
-    subspacetools.md2patchcol(bucknermd, 'brainIso2Ds5Us5size', atlPatchSize, atlLoc, patchColPad);
+    subspacetools.md2patchcol(trainmd, 'brainIso2Ds5Us5sizeReg', atlPatchSize, atlLoc, patchColPad);
 
 % load selected ADNI subject volumes
-fnames = fullfile(SYNTHESIS_DATA_PATH, 'adni', 'md', [sys.usrname, '_restor_md_*']);
-adnimd = loadmd(fnames);
-dsSubjNii = adnimd.loadModality('brainDs5Us5', reconSubj);
+fnames = fullfile(SYNTHESIS_DATA_PATH, testdataset, 'md', [sys.usrname, '_restor_md_*']);
+testmd = loadmd(fnames);
+dsSubjNii = testmd.loadModality('brainDs5Us5', reconSubj);
 dsSubjVol = double(dsSubjNii.img);
-dsSubjWeightVol = logical(adnimd.loadVolume('brainDs5Us5Mask', reconSubj));
-dsSubjInAtlNii = adnimd.loadModality('brainDs5Us5Reg', reconSubj);
-dsSubjInAtlMaskVol = adnimd.loadVolume('brainDs5Us5RegMask', reconSubj);
-subjInAtlTform = load(adnimd.getModality('brainDs5Us5RegMat', reconSubj));
+dsSubjWeightVol = logical(testmd.loadVolume('brainDs5Us5Mask', reconSubj));
+dsSubjInAtlNii = testmd.loadModality('brainDs5Us5Reg', reconSubj);
+dsSubjInAtlMaskVol = testmd.loadVolume('brainDs5Us5RegMask', reconSubj);
+subjInAtlTform = load(testmd.getModality('brainDs5Us5RegMat', reconSubj));
 
 % prepare necessary inputs for conditional-based reconstruction
 subjDims = dsSubjNii.hdr.dime.pixdim(2:4);
@@ -46,7 +50,8 @@ gmmopt = statset('Display', 'iter', 'MaxIter', 20, 'TolFun', 0.001);
 
 % compute the gaussian mixture model
 tic
-gmm = fitgmdist(bucknerIsoPatchCol, gmmK, 'regularizationValue', regVal, 'replicates', 10, 'Options', gmmopt);
+X = bsxfun(@minus, bucknerIsoPatchCol, mean(bucknerIsoPatchCol, 2));
+gmm = fitgmdist(X, gmmK, 'regularizationValue', regVal, 'replicates', 3, 'Options', gmmopt);
 fprintf('Gaussian mixture model took %3.3f sec\n', toc);
 gmm = wgmm.gmdist2wgmm(gmm);
 
@@ -59,6 +64,14 @@ tic
     dsSubjInAtlNii.img, dsSubjInAtlMaskVol, dsSubjVol, dsSubjWeightVol, atlLoc2SubjSpace, extraReconArg);
 toc
 
+%% substitute and repeat
+% TODO: need to substitute in atl. Or will that even make a difference? optimally, we're looking at
+% atl-iso, and that hasn't been fantastic anyway.
+quiltedSubvol
+dsSubjVol
+[quiltedSubvol, reconLoc, cntvol] = papago.subvolRecon(gmm, subvolLoc, subvolSize, atlPatchSize, crmethod, keepr, ...
+    dsSubjInAtlNii.img, dsSubjInAtlMaskVol, dsSubjVol, dsSubjWeightVol, atlLoc2SubjSpace, extraReconArg);
+
 %% test new reconstruction via "pre-computed" R.
 
 R = cor2interpmat(size(atlLoc2SubjSpace{1}), subjLoc2AtlSpace); % move this to pre-computation (?)
@@ -70,8 +83,10 @@ tic;
     dsSubjInAtlNii.img, dsSubjInAtlMaskVol, dsSubjVol, dsSubjWeightVol, atlLoc2SubjSpace, extraReconArg, R);
 toc;
 
+%%
+
 %% visualize
-isoSubjVol = adnimd.loadVolume('brainIso2Ds5Us5size', reconSubj);
+isoSubjVol = testmd.loadVolume('brainIso2Ds5Us5size', reconSubj);
 cropIsoSubjVol = cropVolume(isoSubjVol, reconLoc, reconLoc + size(quiltedSubvol) - 1); 
 cropIsoSubjVol(isnan(quiltedSubvol)) = nan;
 
