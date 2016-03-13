f6338bb09f3bf909c0942c6cc43f5a651b2e929a
adalca <adalca@mit.edu>
2016-03-12 23:13:01 -0500

    examples in doc

--- eval/prepHugeMatfileForVolumeSplitting.m
+++ eval/prepHugeMatfileForVolumeSplitting.m
@@ -1,7 +1,11 @@
 function prepHugeMatfileForVolumeSplitting(mdpath, mod, outmatfile)
-mdpath = [SYNTHESIS_DATA_PATH, '/ADNI_T1_baselines/md/adalca_wholevol_restor_md_2016_03_06.mat']
-mod = 'Ds5Us5RegMask'
-outmatfile = '/data/vision/polina/projects/stroke/work/patchSynthesis/data/ADNI_T1_baselines/subvols/wholevols/mar12_2016/ADNI_T1_baselines_wholevol_Ds5Us5RegMask_volumes.mat'
+% mdpath = [SYNTHESIS_DATA_PATH, '/ADNI_T1_baselines/md/adalca_wholevol_restor_md_2016_03_06.mat']
+% mod = 'Ds5Us5RegMask'
+% outmatfile = '/data/vision/polina/projects/stroke/work/patchSynthesis/data/ADNI_T1_baselines/subvols/wholevol/mar12_2016/ADNI_T1_baselines_wholevol_Ds5Us5RegMask_volumes.mat'
+%
+% mdpath = [SYNTHESIS_DATA_PATH, '/stroke/md/adalca_brain_pad10_restor_md_2016_03_12.mat']
+% mod = 'Ds5Us5RegMask'
+% outmatfile = '/data/vision/polina/projects/stroke/work/patchSynthesis/data/stroke/subvols/brain_pad10/mar12_2016/stroke_brain_pad10_Ds5Us5RegMask_volumes.mat'
 
     load(mdpath);
     
