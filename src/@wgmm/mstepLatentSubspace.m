c864349b4d3a20fb27d12377e55235656fad323d
Adrian Dalca <adalca@mit.edu>
2016-12-02 23:38:35 -0500

    massive mstepLS update

--- src/@wgmm/mstepLatentSubspace.m
+++ src/@wgmm/mstepLatentSubspace.m
@@ -15,6 +15,7 @@ function params = mstepLatentSubspace(wg, data)
     K = data.K;
     Nk = sum(wg.expect.gammank, 1);
     [dHigh, dLow, ~] = size(wg.params.W);
+    assert(dLow == wg.opts.model.dopca);
 
     % initialize new stats
     mu = zeros(K, dHigh);
@@ -23,19 +24,24 @@ function params = mstepLatentSubspace(wg, data)
     
     % do everything *within* each cluster.
     for k = 1:K
+        
+        % compute which gammank are non-trivial for this cluster. For many operations, we only need
+        % to work for those elements where gamma_nk > 0, saving a good amount of computation and,
+        % perhaps more importantly, memory.
+        gEffLog = wg.expect.gammank(:, k) >= 1e-4;
+        gEffIdx = find(gEffLog);
+        gEff = wg.expect.gammank(gEffIdx, k);
+        nEff = numel(gEffIdx);
 
         % update Xhat and Shat in atlas space. 
         %   This should really be part of the E-step, but we'll do it here to
         %   not pass large matrices around. Specifically, here we only need to compute Shat and Xhat
         %   for one cluster at a time.
-        Shat = zeros(dLow, dLow, N);
+        Shat = zeros(dLow, dLow, nEff);
         Xhat = zeros(N, dLow);
-        
-        % go through each point
-        for i = 1:N
-            % don't need to compute Xhat_ki, Shat_ki when gnk is ~0
-            % since they're only used when multiplied by gnk
-            if wg.expect.gammank(i, k) < 1e-4, continue; end 
+               
+        for ei = 1:nEff
+            i = gEffIdx(ei);
             
             % extract observed entries
             obsIdx = obsMask(i, :);
@@ -51,16 +57,17 @@ function params = mstepLatentSubspace(wg, data)
             %   S_ki = inv(L * L' + eye(dLow));
             ltl = L'*L;
             Ski = eye(dLow) - ltl / (eye(dLow) + ltl);
-            Shat(:, :, i) = Ski;
-            Xhat(i, :) = Ski/vk * (w' * (yobs - wg.params.mu(k, obsIdx))');
+            Shat(:, :, ei) = Ski;
+            Xhat(ei, :) = Ski/vk * (w' * (yobs - wg.params.mu(k, obsIdx))');
         end
+        assert(isclean(Xhat));
+        assert(isclean(Shat));
 
         % update mu
         muDenom = zeros(1, dHigh);
-        for i = 1:N
-            % don't need to compute Xhat_ki, Shat_ki, since they're only used when gamma_ki is > 0
-            gnk = wg.expect.gammank(i, k);
-            if gnk < 1e-4, continue; end 
+        for ei = 1:nEff
+            i = gEffIdx(ei);
+            gnk = gEff(ei);
             
             % extract available voxels
             obsIdx = obsMask(i, :);
@@ -68,7 +75,7 @@ function params = mstepLatentSubspace(wg, data)
 
             % extrace necessary data
             w = wg.params.W(obsIdx, :, k);
-            X_ki = Xhat(i, :)';
+            X_ki = Xhat(ei, :)';
 
             % update mu
             muterm = yobs' - w * X_ki;
@@ -89,21 +96,20 @@ function params = mstepLatentSubspace(wg, data)
         % update W
         %   first, pre-compute (Shat * gammank), much faster than re-computing that operation for
         %   overlapping subsets of Shat inside the loop!
-        gammank = permute(wg.expect.gammank(:, k), [2, 3, 1]);
-        gShatFull = bsxfun(@times, Shat, gammank);
+        gShatFull = bsxfun(@times, Shat, permute(gEff, [2,3,1]));
         for j = 1:dHigh
             % extract observed data indices
-            % note: using gnk >= 1e-4
-            lobsIdx = obsMask(:, j) & (wg.expect.gammank(:, k) >= 1e-4);
+            lobsIdx = obsMask(gEffIdx, j); 
+            yobs = Y(gEffIdx(lobsIdx), j);
 
             % extract wanted data
-            gnk = wg.expect.gammank(lobsIdx, k);
+            gnk = gEff(lobsIdx);
             xhat = Xhat(lobsIdx, :);
             gShat = gShatFull(:, :, lobsIdx); % Much faster this way: use pre-computed Shat*g
             
             % compute numerator and denominator
             sgXhat = bsxfun(@times, sqrt(gnk), xhat);
-            wNum = xhat' * (gnk .* (Y(lobsIdx, j) - mu(k, j))); % low-dim centered data
+            wNum = xhat' * (gnk .* (yobs - mu(k, j))); % low-dim centered data
             wDenom = sgXhat' * sgXhat + sum(gShat, 3); % low-dim variance
             
             % update W
@@ -113,12 +119,12 @@ function params = mstepLatentSubspace(wg, data)
             warning('mstepLS: found %d NANs in W_%d! Filling them in randomly', sum(sum(isnan(newW(:, :, k)))), k);
             newW(isnan(newW)) = randn(sum(isnan(newW(:))), 1);
         end
-            
+        
         % update residual variance
-        v = zeros(dHigh, 1);
-        for i = 1:N
-            gnk = wg.expect.gammank(i, k);
-            if gnk < 1e-4, continue; end
+        v = 0;
+        for ei = 1:nEff
+            i = gEffIdx(ei);
+            gnk = gEff(ei);
             
             % extract observed
             obsIdx = obsMask(i, :);
@@ -126,27 +132,27 @@ function params = mstepLatentSubspace(wg, data)
 
             % new w
             w = newW(obsIdx, :, k);
-            xhat = Xhat(i, :);
+            xhat = Xhat(ei, :);
             
             % update terms
-            resTerm = gnk .* (yobs - xhat * w' - mu(k, obsIdx)) .^ 2;
-            varTerm = gnk .* diag(w * Shat(:, :, i) * w');
-
-            v(obsIdx) = v(obsIdx) + resTerm(:) + varTerm(:);
+            resTerm = sqrt(gnk) .* (yobs - xhat * w' - mu(k, obsIdx)); % will square below
+            % want to compute the term sum(gnk .* diag(w * Shat(:, :, ei) * w');)
+            % which is really the trace, so we can change out multiplications around to be faster.
+            m1 = w' * w * Shat(:, :, ei); % fastest version for trace.
+            varTerm = gnk .* trace(m1);
+            
+            v = v + resTerm * resTerm' + varTerm;
         end
-        newsigmasq(k) = sum(v) ./ sum(muDenom);
+        newsigmasq(k) = v ./ sum(muDenom);
         assert(isclean(newsigmasq));
+        
     end
         
     % update parameters
     params.mu = mu;
     params.W = newW;
     params.sigmasq = newsigmasq;
-    sigma = zeros(dHigh, dHigh, K);
-    for k = 1:K
-        sigma(:,:,k) = newW(:,:,k) * newW(:,:,k)' + newsigmasq(k) .* eye(dHigh);
-    end
-    [params.sigma, params.sigmainv] = wgmm.sigmafull(wg, sigma);
+
     
     % pi update
     params.pi = Nk ./ N; 
