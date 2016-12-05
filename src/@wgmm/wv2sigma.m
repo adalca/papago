5466cd1a0789fc5bd2399ad8206ebd0b4dc9c045
adalca <adalca@mit.edu>
2016-12-05 16:44:25 -0500

    fixing wv2sigma took

--- src/@wgmm/wv2sigma.m
+++ src/@wgmm/wv2sigma.m
@@ -1,6 +1,9 @@
-function wgs = wv2sigma(wgs)
+function sigma = wv2sigma(wgs)
 
+    [dHigh, ~, K] = size(wgs.params.W);
+
+    sigma = zeros(dHigh, dHigh, K);
     for k = 1:size(wgs.params.W, 3)
-        wgs.params.sigma(:,:,k) = wgs.params.W(:,:,k) * wgs.params.W(:,:,k)' + ...
+        sigma(:,:,k) = wgs.params.W(:,:,k) * wgs.params.W(:,:,k)' + ...
             wgs.params.sigmasq(k) * eye(size(wgs.params.W, 1));
     end
