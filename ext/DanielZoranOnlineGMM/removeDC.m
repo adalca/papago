7ff840678ae01a0d44fd28fa24ddf984c59cd73f
adalca <adalca@mit.edu>
2017-08-11 18:08:17 -0400

    move files to papagoTest

--- ext/DanielZoranOnlineGMM/removeDC.m
+++
@@ -1,3 +0,0 @@
-function [X,ns,xs,ys] = removeDC(X,ns,xs,ys)
-
-X = X - repmat(mean(X,1), [size(X,1) 1]); 
--- test/VolumeRegistrationExampleCont.m
+++
@@ -1,8 +0,0 @@
-% [214, 264, 13] from moving image should match [99 133 13] in fixed image
-% or [129 253 13] --> [50 128 12]
-% seems to roughly work!!!
-
-[w.y, w.x, w.z] = Rmoving.intrinsicToWorld(253, 129, 13);
-[t.y, t.x, t.z] = transformPointsForward(geomtform, w.y, w.x, w.z);
-[f.y, f.x, f.z] = Rfixed.worldToIntrinsic(t.y, t.x, t.z);
-f
