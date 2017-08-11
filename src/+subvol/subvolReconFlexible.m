8742de13f20022ae9630d2060637ac28240e9fe6
Adrian Dalca <adalca@salep.csail.mit.edu>
2017-08-11 17:36:46 -0400

    adding corrections to patch rotation

--- src/+subvol/subvolReconFlexible.m
+++ src/+subvol/subvolReconFlexible.m
@@ -17,7 +17,7 @@ function subvolReconFlexible(wgmmFile, atlSubvols, iniReconFile, atlSubjVol, atl
     if ~sys.isfile(atlSubvols)
         % assume it's a list
         atlFiles = strsplit(atlSubvols, ',');
-        outFiles = strsplit(atlSubvols, ',');
+        outFiles = strsplit(subjoutFile, ',');
         wgmmFiles = strsplit(wgmmFile, ',');
         
         assert(numel(atlFiles) == numel(outFiles), ...
@@ -30,8 +30,8 @@ function subvolReconFlexible(wgmmFile, atlSubvols, iniReconFile, atlSubjVol, atl
     end
     
     % load subvolume location
-    q = load(atlSubvols);
-    reconLoc = q.nfo.subvolLoc;
+    q = load(atlSubvols, 'nfo');
+    atlReconLoc = q.nfo.subvolLoc;
     atlSubvolSize = q.nfo.subvolSize;
     
     % get other ingo.
@@ -40,28 +40,70 @@ function subvolReconFlexible(wgmmFile, atlSubvols, iniReconFile, atlSubjVol, atl
     reconModel = ini.reconModel;
     
     % load volumes
-    q = load(wgmmFile);
+    q = load(wgmmFile, 'wg');
     wg = q.wg;
+    wg.params.sigma = wg.wv2sigma;
     
     % get rotation data for this subvolume. do NOT pass in an output file -- we just want this
     % returned. This loads a signle volume, despite the plural (e.g. subvols) in the variable names.
-    [interpData.subVols, interpData.subVolMasks, interpData.atl2SubjInterpMat, interpData.subj2AtlInterpMat] = ...
-        vol2subvolInterpData(interpMatData, atlSubjVol, atlSubjMask, reconLoc, atlSubvolSize);
+    [a, b, c, d, rangeMins] = ...
+        vol2subvolInterpData(interpMatData, atlSubjVol, atlSubjMask, atlReconLoc, atlSubvolSize);
+    interpData.subVols = {a};
+    interpData.subVolMasks = {b}; 
+    interpData.atl2SubjInterpMat = {c};
+    interpData.subj2AtlInterpMat = {d};
     
     % get R matrices.
-    interpDataPatches = interpData2patchRotData(interpData, subvolSize, 0, atlPatchSize, 1);
+    [interpDataPatches, availableData] = interpData2patchRotData(interpData, atlSubvolSize, 0, atlPatchSize, 1);
+    
+    % clean up non-completed patches
+    if not(all(availableData(:)))
+        fprintf('Using %d available patches\n', sum(availableData(:)));
+        assert(any(availableData(:)), 'Could not find available patch');
+        interpDataPatches.lenR = interpDataPatches.lenR(availableData, :);
+        interpDataPatches.yorigAll = interpDataPatches.yorigAll(availableData, :);
+        interpDataPatches.yrotmasksAll = interpDataPatches.yrotmasksAll(availableData, :);
+        interpDataPatches.ydsmasksAll = interpDataPatches.ydsmasksAll(availableData, :);
+        interpDataPatches.boundingBoxes = interpDataPatches.boundingBoxes(availableData, :);
+        interpDataPatches.completed = interpDataPatches.completed(availableData, :);
+        interpDataPatches.rWeightAll = interpDataPatches.rWeightAll(availableData, :);
+        interpDataPatches.ydsmasksAllFullVoxels = interpDataPatches.ydsmasksAllFullVoxels(availableData, :);
+    end
 
     % reconstruct patches
+    nanmaskvox2volfn = @(a,b) maskvox2vol(a,b, @nan);
     nPatches = numel(interpDataPatches.yorigAll);
-    volRotData = paffine.prepareWgmmRData(interpDataPatches, ones(1, nPatches));
+    volRotData = paffine.prepareWgmmRData(interpDataPatches, 1:nPatches);
+    volRotData.K = numel(wg.params.pi);
     reconPatches = wg.recon(volRotData, reconModel); %'latentMissingR');
+%     reconPatches = cellfunc(@(x) x(:), reconPatches);
     
     % reconstruct subvolume
     rotmasks = interpDataPatches.yrotmasksAll(:);
-    patches = cellfunc(@maskvox2vol, reconPatches(:), rotmasks);
+    patches = cellfunc(nanmaskvox2volfn, reconPatches(:), rotmasks);
+    dsmasks = cellfunc(@maskvox2vol, interpDataPatches.ydsmasksAll(:), rotmasks);
     recmasks = cellfunc(@maskvox2vol, volRotData.rWeight, rotmasks);
+    recmasks = cellfunc(@(rm, dm) nanmax(rm, dm), recmasks, dsmasks);
+    for pi = 1:numel(patches), recmasks{pi}(isnan(patches{pi})) = 0; end
+    volStarts = cellfunc(@(x) x(1:3), interpDataPatches.boundingBoxes);  
+    
+    % determine what region of the subvolume is quilted using the reconPatches
+        
+%     reconLocsEnd = cellfunc(@(x, y) x  + size(y), ?, patches); 
+%     minSubvolLoc = min(cat(1, ?{:}), [], 1);
+    minSubvolLoc = rangeMins;
+%     maxSubvolLoc = max(cat(1, reconLocsEnd{:}), [], 1);
+    patchSizes = cellfunc(@(y) [size(y, 1), size(y, 2), size(y, 3)], patches); % force 3d
+    relReconLocsEnd = cellfunc(@(x, y) x  + y, volStarts, patchSizes); 
+    subvolAtlSize = max(cat(1, relReconLocsEnd{:}));
+    
+    % determine the size of the subvolume that will be quilted
+%     subvolAtlSize = maxSubvolLoc - minSubvolLoc + 1; 
+    
     [reconVol, reconWeight] = patchlib.quiltIrregularPatches(volStarts, patches, ...
-        'volSize', atlSubvolSize, 'weightPatches', recmasks);
+        'volSize', subvolAtlSize, 'weightPatches', recmasks);
 
+    reconLoc = minSubvolLoc; 
+    
     % save reconPatches.
     save(subjoutFile, 'reconVol', 'reconLoc', 'reconWeight');
