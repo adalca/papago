61411468e51ad5d15a3c29f0d295d6eeb06fd4ed
Adrian Dalca <adalca@salep.csail.mit.edu>
2017-08-11 17:37:01 -0400

    updates to quilting subvols

--- src/tools/quiltSubvols.m
+++ src/tools/quiltSubvols.m
@@ -1,19 +1,28 @@
-function quiltSubvols(subVolPath, quiltVolumeFilename)
+function [vol, countvol] = quiltSubvols(subVolPath, quiltVolumeFilename)
 
-d = sys.fulldir(subVolPath); 
+    d = sys.fulldir(subVolPath); 
+    fprintf('Found %d subvolumes\n', numel(d));
+    
+    if numel(d) == 0
+        vol = [];
+        countvol = [];
+    else
 
-reconVols = cell(1, numel(d)); 
-reconLocs = reconVols; 
-reconWeights = reconVols; 
+        reconVols = cell(1, numel(d)); 
+        reconLocs = reconVols; 
+        reconWeights = reconVols; 
 
-for i = 1:numel(d)
-    q=load(d(i).name); 
-    reconVols{i}=q.reconVol; 
-    reconLocs{i}=q.reconLoc; 
-    reconWeights{i}=q.reconWeight; 
-end
+        for i = 1:numel(d)
+            q=load(d(i).name); 
+            reconVols{i}=q.reconVol; 
+            reconLocs{i}=q.reconLoc; 
+            reconWeights{i}=q.reconWeight; 
+        end
 
 
-[vol, countvol] = patchlib.quiltIrregularPatches(reconLocs, reconVols, 'weightPatches', reconWeights);
-
-save(quiltVolumeFilename, 'vol', 'countvol'); 
+        [vol, countvol] = patchlib.quiltIrregularPatches(reconLocs, reconVols, 'weightPatches', reconWeights);
+    end
+    
+    if exist('quiltVolumeFilename', 'var')
+        save(quiltVolumeFilename, 'vol', 'countvol'); 
+    end    
