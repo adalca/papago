beda391c44361f90d380cde407b60379872ad016
Adrian Dalca <adalca@salep.csail.mit.edu>
2017-08-11 17:34:06 -0400

    temp hack to not load G

--- src/+paffine/prepareWgmmRData.m
+++ src/+paffine/prepareWgmmRData.m
@@ -1,5 +1,6 @@
 function q = prepareWgmmRData(interpDataPatches, dataridx)
 
+    warning('Taking G out of prepareWgmmRData');
 
     % get indices from lengths 
     ridx = lenMatrix2idx(interpDataPatches.lenR);
@@ -7,10 +8,10 @@ function q = prepareWgmmRData(interpDataPatches, dataridx)
     allridx = cat(2, ridx{dataridx});
     
     q.R.data = interpDataPatches.allR(allridx, :);
-    gidx = lenMatrix2idx(interpDataPatches.lenG);
-    q.G.data = interpDataPatches.allG(:, cat(2, gidx{dataridx}));
+%     gidx = lenMatrix2idx(interpDataPatches.lenG);
+%     q.G.data = interpDataPatches.allG(:, cat(2, gidx{dataridx}));
     q.R.idx = lenMatrix2idx(interpDataPatches.lenR(dataridx));
-    q.G.idx = lenMatrix2idx(interpDataPatches.lenG(dataridx));
+%     q.G.idx = lenMatrix2idx(interpDataPatches.lenG(dataridx));
     
     q.Y = interpDataPatches.yorigAll(dataridx);
     q.ydsmasks = interpDataPatches.ydsmasksAll(dataridx);
