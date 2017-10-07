065b68a53944659aa506ed90a6fc1aea35973df2
Adrian Dalca <adalca@mit.edu>
2017-10-07 19:47:33 -0400

    add mcc rand

---
+++ mcc/mccSubvol2randWLSwgmm.m
@@ -0,0 +1,3 @@
+function mccSubvol2randWLSwgmm(dsSubvolMat, wtSubvolMat, clusterIdxMat, wgmmMat, iniFilename)
+
+    subvol.subvol2randWLSwgmm(dsSubvolMat, wtSubvolMat, clusterIdxMat, wgmmMat, iniFilename);
